2cfde4ededfc6693614361e48aed7528e882cd54
Christian Chomiak <christianchomiak@gmail.com>
2014-01-25 17:40:44 -0430

    Basic Mechanics and some texture

--- Pike and Boulder/Assets/Player.cs
+++ Pike and Boulder/Assets/Player.cs
@@ -18,79 +18,27 @@ public class Player : MonoBehaviour {
     public float fatRadius, slimRadius;
     public Camera theCamera;
 
+    public float fatWalkSpeed, slimWalkSpeed;
+    public float fatJump, slimJump;
+
+    public float fatAspect, slimAspect;
+
+    public float fatPushPower, slimPushPower;
+
+    float pushPower;
+
     // Use this for initialization
     void Start()
     {
         currentTimer = 0.5f;
 
-        // set the desired aspect ratio (the values in this example are
-        // hard-coded for 16:9, but you could make them into public
-        // variables instead so you can set them at design time)
-        float targetaspect = desiredAspectW / desiredAspectH; // 16.0f / 9.0f;
-
-        // determine the game window's current aspect ratio
-        float windowaspect = (float)Screen.width / (float)Screen.height;
-
-        // current viewport height should be scaled by this amount
-        float scaleheight = windowaspect / targetaspect;
-
-        // obtain theCamera component so we can modify its viewport
-        //theCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
-
         if (isFat)
         {
-            //theCamera.aspect = 12.9f / 9f;
-            //targetAspect = 12.9f;
-            minAspectTarget = 24f;
-            maxAspectTarget = 12.9f;
-            currentTimer = 0;
-            isFat = false;
-
-            fatWorker.localScale = fatWorkerScale / 10f; // new Vector3(0.6666f, 1, 0.6666f);
-            GetComponent<CharacterController>().radius = slimRadius;
+            GetFat();
         }
         else if (!isFat)
         {
-            //theCamera.aspect = 24f / 9f;
-            //targetAspect = 24f;
-            minAspectTarget = 12.9f;
-            maxAspectTarget = 24f;
-            currentTimer = 0;
-            isFat = true;
-            fatWorker.localScale = fatWorkerScale; // new Vector3(1.5f, 1, 1.5f);
-            GetComponent<CharacterController>().radius = fatRadius;
-        }
-
-        if (true)
-        {
-            theCamera.aspect = targetaspect;
-            return;
-        }
-
-        // if scaled height is less than current height, add letterbox
-        if (scaleheight < 1.0f)
-        {
-            Rect rect = theCamera.rect;
-
-            rect.width = 1.0f;
-            rect.height = scaleheight;
-            rect.x = 0;
-            rect.y = (1.0f - scaleheight) / 2.0f;
-
-            theCamera.rect = rect;
-        }
-        else // add pillarbox
-        {
-            float scalewidth = 1.0f / scaleheight;
-
-            Rect rect = theCamera.rect;
-
-            rect.width = scalewidth;
-            rect.height = 1.0f;
-            rect.x = (1.0f - scalewidth) / 2.0f;
-            rect.y = 0;
-
-            theCamera.rect = rect;
+            GetSlim();
         }
     }
 
@@ -116,25 +64,61 @@ public class Player : MonoBehaviour {
         {
             //theCamera.aspect = 12.9f / 9f;
             //targetAspect = 12.9f;
-            minAspectTarget = 24f;
-            maxAspectTarget = 12.9f;
-            currentTimer = 0;
-            isFat = false;
 
-            fatWorker.localScale = fatWorkerScale / 10f; // new Vector3(0.6666f, 1, 0.6666f);
-            GetComponent<CharacterController>().radius = slimRadius;
+            GetSlim();
         }
         else if (!isFat && Input.GetKey(KeyCode.RightControl))
         {
             //theCamera.aspect = 24f / 9f;
             //targetAspect = 24f;
-            minAspectTarget = 12.9f;
-            maxAspectTarget = 24f;
-            currentTimer = 0;
-            isFat = true;
-            fatWorker.localScale = fatWorkerScale; // new Vector3(1.5f, 1, 1.5f);
-            GetComponent<CharacterController>().radius = fatRadius;
+            GetFat();
         }
         //theCamera.aspect = desiredAspectW / desiredAspectH;	
     }
+
+    void GetSlim()
+    {
+        minAspectTarget = fatAspect;
+        maxAspectTarget = slimAspect;
+        currentTimer = 0;
+        isFat = false;
+
+        fatWorker.localScale = fatWorkerScale / 10f; // new Vector3(0.6666f, 1, 0.6666f);
+        GetComponent<CharacterController>().radius = slimRadius;
+
+        GetComponent<ThirdPersonController>().walkSpeed = slimWalkSpeed;
+        GetComponent<ThirdPersonController>().jumpHeight = slimJump;
+
+        pushPower = slimPushPower;
+    }
+
+    void GetFat()
+    {
+        minAspectTarget = slimAspect;
+        maxAspectTarget = fatAspect;
+        currentTimer = 0;
+        isFat = true;
+        fatWorker.localScale = fatWorkerScale; // new Vector3(1.5f, 1, 1.5f);
+        GetComponent<CharacterController>().radius = fatRadius;
+
+        GetComponent<ThirdPersonController>().walkSpeed = fatWalkSpeed;
+        GetComponent<ThirdPersonController>().jumpHeight = fatJump;
+
+        pushPower = fatPushPower;
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+
+        Rigidbody body = hit.collider.attachedRigidbody;
+        if (body == null || body.isKinematic)
+            return;
+
+        if (hit.moveDirection.y < -0.3F)
+            return;
+
+        Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+        body.velocity = pushDir * pushPower;
+    }
+
 }
